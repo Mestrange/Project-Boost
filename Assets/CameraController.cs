eea0b4695c8ddc0a4d63ac79d583225ecc4d8d3f
Snickers <merk.ammv@gmail.com>
2018-08-25 17:18:37 +0300

    Камера следует за игроком

---
+++ Assets/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public GameObject player;
+    private Vector3 offsetY;
+    private Vector3 offsetX;
+    private Vector3 offset;
+    // Use this for initialization
+    void Start ()
+    {
+        //transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+        transform.position = player.transform.position + offset;
+        /*  transform.up = transform.up + offsetY;
+          transform.right = transform.right + offsetX;*/
+        // CameraMovement();
+    }
+
+    private void CameraMovement()
+    {
+       
+        
+    }
+
+}
